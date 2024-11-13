7c43fa0146a13f8722589871d794d78dd28930e4
hanyc91539 <hanyc91539@gmail.com>
2024-11-14 02:48:05 +0900

    added CBA league

--- Leagues.py
+++ Leagues.py
@@ -40,5 +40,21 @@ leagues = [
                             },
         "Halftime":     {"status": True , "odds": 1.65, "history_count": 5, "score": 1},
         "QuarterML":    {"status": False, "score": 4}
+    },
+    {
+        "short_name": "CBA",
+        "name": "Chinese Basketball Association",
+        "id": "ngy0or5gteqwzv3",
+        "url": "https:stake.ac/sports/basketball/china/cba",
+        "time": 12,
+        "round": 4,
+        "QuarterTotal": {"status": False,
+                            "1": {"rain": 11, "rain better": 50, "drought": 64, "drought better": 600},
+                            "2": {"rain": 11, "rain better": 50, "drought": 68, "drought better": 600},
+                            "3": {"rain": 11, "rain better": 50, "drought": 68, "drought better": 600},
+                            "4": {"rain": 11, "rain better": 50, "drought": 70, "drought better": 600}
+                            },
+        "Halftime":     {"status": True , "odds": 1.65, "history_count": 5, "score": 1},
+        "QuarterML":    {"status": False, "score": 4}
     }
 ]
